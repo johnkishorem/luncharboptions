d522a962e09a41f5d8d93af17e4b166c7f1c3115
John Kishore M <johnkishorem@gmail.com>
2018-03-11 16:51:18 +0530

    logout functionality completed

--- polls/urls.py
+++ polls/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
-from .views import index
+from polls.views import index,logout_view
 
 urlpatterns = [
     url(r'^$', 	index),
-    url(r'^login/$', auth_views.login, {'template_name': 'polls/login.html'})
+    url(r'^login/$', auth_views.login, {'template_name': 'polls/login.html'}, name='login'),
+    url(r'^logout/$', logout_view, name='logout'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import logout
+from django.urls import reverse
 
 # Create your views here.
 
@@ -11,4 +12,9 @@ def index(request):
 	if request.user.is_authenticated:
 		return HttpResponse("Hello " + request.user.username)
 	else:
-		return HttpResponseRedirect('login/')
+		return HttpResponseRedirect(reverse('login'))
+
+
+def logout_view(request):
+	logout(request)
+	return HttpResponseRedirect(reverse('login'))
