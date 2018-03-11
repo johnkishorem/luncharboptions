6715fe069c8faf7200ce7963cfb321391bb79c30
John Kishore M <johnkishorem@gmail.com>
2018-03-11 13:09:45 +0530

    Index view added to polls app

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.contrib.auth import views as auth_views
+from .views import index
 
 urlpatterns = [
+    url(r'^$', 	index),
     url(r'^login/$', auth_views.login, {'template_name': 'polls/login.html'})
 ]
