b63812c7edcce2ce3a523d7b698556706c11b06f
John Kishore M <johnkishorem@gmail.com>
2018-03-11 10:16:46 +0530

    Models added for polls

--- polls/admin.py
+++ polls/admin.py
@@ -2,5 +2,9 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from models import hotel_option, time_option, arb_option
 
 # Register your models here.
+admin.site.register(hotel_option)
+admin.site.register(time_option)
+admin.site.register(arb_option)
--- polls/models.py
+++ polls/models.py
@@ -2,5 +2,33 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Models for represent hotel options
+class hotel_option(models.Model):
+	hotel_name = models.CharField(max_length=200)
+	hotel_location = models.CharField(max_length=200)
+	hotel_avg_budget = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.hotel_name
+
+class time_option(models.Model):
+	time_slot = models.CharField(max_length=50)
+
+	def __str__(self):
+		return self.time_slot
+
+
+class arb_option(models.Model):
+	arb_user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+	arb_hotel_option = models.ForeignKey(hotel_option, on_delete=models.CASCADE, null=True)
+	arb_time_option = models.ForeignKey(time_option, on_delete=models.CASCADE, null=True)
+	arb_pub_date = models.DateField(auto_now=False, auto_now_add=True)
+
+	def __str__(self):
+		user = str(self.arb_user.username)
+		hotel_name = str(self.arb_hotel_option.hotel_name)
+		time_slot = str(self.arb_time_option.time_slot)
+		date = str(self.arb_pub_date)
+		return "%s recommended %s at %s on %s" % (user, hotel_name, time_slot, date)
