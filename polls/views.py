5ab11cefdd487ffcd3303b337feeb22aa6f9eeec
John Kishore M <johnkishorem@gmail.com>
2018-03-11 14:11:38 +0530

    Log out added for testing

--- polls/views.py
+++ polls/views.py
@@ -3,11 +3,13 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.contrib.auth import logout
 
 # Create your views here.
 
 def index(request):
 	if request.user.is_authenticated:
+		logout(request)
 		return HttpResponse("Hello " + request.user.username)
 	else:
-		return HttpResponseRedirect('/login')
+		return HttpResponseRedirect('login/')
