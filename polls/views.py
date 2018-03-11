6b1e532c6310c382bef1621d41150bbc90d566db
John Kishore M <johnkishorem@gmail.com>
2018-03-11 13:56:23 +0530

    Redirect added to unauthenticated user

--- polls/views.py
+++ polls/views.py
@@ -2,9 +2,12 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Hello world")
+	if request.user.is_authenticated:
+		return HttpResponse("Hello " + request.user.username)
+	else:
+		return HttpResponseRedirect('/login')
