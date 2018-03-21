c7de4311a38836f0cf9c7302df2e775f8df30490
John Kishore M <johnkishorem@gmail.com>
2018-03-22 01:07:09 +0530

    Use Authentication class as views

--- polls/urls.py
+++ polls/urls.py
@@ -5,7 +5,7 @@ from polls.views import index_view,logout_view,vote_view,register_view
 app_name = 'polls'
 urlpatterns = [
     url(r'^$', 	index_view, name='index'),
-    url(r'^login/$', auth_views.login, {'template_name': 'polls/login.html'}, name='login'),
+    url(r'^login/$', auth_views.LoginView.as_view(template_name='polls/login.html'), name='login'),
     url(r'^register/$', register_view, name='register'),
     url(r'^logout/$', logout_view, name='logout'),
     url(r'^vote/$', vote_view, name='vote'),
